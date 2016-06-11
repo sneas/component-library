5509dd3cebf6ef9cb002a71ef5ec7645be288e9a
Dima Snisarenko <snisarenkodima@gmail.com>
2016-06-11 15:30:59 +0200

    Add main navigation menu

--- assets/js/cl.js
+++ assets/js/cl.js
@@ -1,5 +1,41 @@
 'use strict';
 
+(function (window, document) {
+
+    var layout   = document.getElementById('cl-layout'),
+        menu     = document.getElementById('cl-menu'),
+        menuLink = document.getElementById('cl-menuLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+            if (classes[i] === className) {
+                classes.splice(i, 1);
+                break;
+            }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    };
+
+}(this, this.document));
+
 jQuery(function($) {
     $('.cl-menu-toggle-button').click(function() {
         function toggle(visible) {
