fa1a952a68713aa4736ffb7800ed424a60fc5d93
Dima Snisarenko <snisarenkodima@gmail.com>
2016-06-12 10:37:48 +0200

    Prepare for publish

---
+++ demo/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    path = require('path'),
+    //componentsLibrary = require('components-library')
+    componentsLibrary = require('../index.js');
+
+gulp.task('compile', function() {
+    componentsLibrary({
+        patternsDir: path.format({dir: __dirname, base: 'templates'}),
+        outputDir: path.format({dir: __dirname, base: 'public'}),
+        baseUrl: '/',
+        js: [
+            '/external.js'
+        ],
+        css: [
+            '/external.css'
+        ]
+    });
+});
+
+gulp.task('watch', ['compile'], function() {
+    gulp.watch(['templates/**/*', '../jade/**/*', '../sass/**/*', '../assets/**/*'], ['compile']);
+});
