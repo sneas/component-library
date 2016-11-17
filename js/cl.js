8be64381dfd71102627931bfae6c30f54935f8bb
Dima Snisarenko <snisarenkodima@gmail.com>
2016-11-17 15:00:20 +0100

    Refactor existing code to use Babel and Nunjucks

--- js/cl.js
+++ js/cl.js
@@ -3,10 +3,11 @@
 /**
  * Show/hide mobile menu functionality
  */
+
 (function (window, document) {
 
-    var layout   = document.getElementById('cl-layout'),
-        menu     = document.getElementById('cl-menu'),
+    var layout = document.getElementById('cl-layout'),
+        menu = document.getElementById('cl-menu'),
         menuLink = document.getElementById('cl-menuLink');
 
     function toggleClass(element, className) {
@@ -14,7 +15,7 @@
             length = classes.length,
             i = 0;
 
-        for(; i < length; i++) {
+        for (; i < length; i++) {
             if (classes[i] === className) {
                 classes.splice(i, 1);
                 break;
@@ -36,39 +37,37 @@
         toggleClass(menu, active);
         toggleClass(menuLink, active);
     };
-
-}(window, document));
+})(window, document);
 
 /**
  * Show/hide code functionality
  */
-(function(document) {
-    document.addEventListener("DOMContentLoaded", function() {
-        document.querySelectorAll('.cl-js-template-code').forEach(function(templateBlock) {
+(function (document) {
+    document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll('.cl-js-template-code').forEach(function (templateBlock) {
             var templateCode = templateBlock.querySelector('.cl-js-template-code-content'),
                 activeAttribute = 'cl-visible';
-            templateBlock.querySelector('.cl-js-template-code-toggle').onclick = function() {
+            templateBlock.querySelector('.cl-js-template-code-toggle').onclick = function () {
                 if (templateCode.hasAttribute(activeAttribute)) {
                     templateCode.removeAttribute(activeAttribute);
                 } else {
                     templateCode.setAttribute(activeAttribute, true);
                 }
-            }
+            };
         });
     });
 })(document);
 
-
 /**
  * Search functionality
  */
-(function(document) {
+(function (document) {
     var searchInputId = 'cl-search',
         listId = 'cl-navigation-list',
         hiddenAttribute = 'cl-hidden',
         searchInput = document.getElementById(searchInputId);
 
-    searchInput.addEventListener('keyup', function() {
+    searchInput.addEventListener('keyup', function () {
         var searchString = searchInput.value;
         if (searchString.length <= 1) {
             unHideAll();
@@ -77,22 +76,17 @@
 
         searchString = searchString.replace("'", "\\'");
 
-        var query = "//ul[@id='" + listId + "']//a[text()[not(contains(translate(., '" +
-            searchString.toUpperCase() + "', '" +
-            searchString.toLowerCase() + "'), '" +
-            searchString.toLowerCase() + "'))]]";
+        var query = "//ul[@id='" + listId + "']//a[text()[not(contains(translate(., '" + searchString.toUpperCase() + "', '" + searchString.toLowerCase() + "'), '" + searchString.toLowerCase() + "'))]]";
 
-        var nodesSnapshot = document.evaluate(query, document,null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        var nodesSnapshot = document.evaluate(query, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-        for ( var i=0 ; i < nodesSnapshot.snapshotLength; i++ )
-        {
+        for (var i = 0; i < nodesSnapshot.snapshotLength; i++) {
             nodesSnapshot.snapshotItem(i).setAttribute(hiddenAttribute, true);
         }
     });
 
-    function unHideAll()
-    {
-        document.querySelectorAll('#' + listId + ' [' + hiddenAttribute + ']').forEach(function(hiddenElement) {
+    function unHideAll() {
+        document.querySelectorAll('#' + listId + ' [' + hiddenAttribute + ']').forEach(function (hiddenElement) {
             hiddenElement.removeAttribute(hiddenAttribute);
         });
     }
