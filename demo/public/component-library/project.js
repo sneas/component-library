45e742d353ee4056e8805675c03770c03e8b46fc
Dima Snisarenko <snisarenkodima@gmail.com>
2016-11-20 21:27:11 +0100

    Exclude generated demo from git

--- demo/public/component-library/project.js
+++
@@ -1,8 +0,0 @@
-//"use strict";
-(function(document){
-    Array.prototype.forEach.call(document.getElementsByTagName('textarea'), function(element) {
-        element.onclick = function() {
-            alert('Element is clicked!');
-        }
-    });
-})(document);
