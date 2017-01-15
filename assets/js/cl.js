d7e34ecca50aade99d0cab7e9edb77330f10c628
Dima Snisarenko <snisarenkodima@gmail.com>
2017-01-15 18:53:54 +0100

    feat: Utilize bootstrap navigation

--- assets/js/cl.js
+++ assets/js/cl.js
@@ -1,46 +1,7 @@
 'use strict';
 
 /**
- * Show/hide mobile menu functionality
- */
-(function (window, document) {
-
-    var layout   = document.getElementById('cl-layout'),
-        menu     = document.getElementById('cl-menu'),
-        menuLink = document.getElementById('cl-menuLink');
-
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
-
-        for(; i < length; i++) {
-            if (classes[i] === className) {
-                classes.splice(i, 1);
-                break;
-            }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
-    }
-
-    menuLink.onclick = function (e) {
-        var active = 'active';
-
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
-    };
-
-}(window, document));
-
-/**
- * Show/hide code functionality
+ * Show/hide code
  */
 (function(document) {
     document.addEventListener("DOMContentLoaded", function() {
@@ -56,44 +17,4 @@
             }
         });
     });
-})(document);
-
-
-/**
- * Search functionality
- */
-(function(document) {
-    var searchInputId = 'cl-search',
-        listId = 'cl-navigation-list',
-        hiddenAttribute = 'cl-hidden',
-        searchInput = document.getElementById(searchInputId);
-
-    searchInput.addEventListener('keyup', function() {
-        var searchString = searchInput.value;
-        if (searchString.length <= 1) {
-            unHideAll();
-            return;
-        }
-
-        searchString = searchString.replace("'", "\\'");
-
-        var query = "//ul[@id='" + listId + "']//a[text()[not(contains(translate(., '" +
-            searchString.toUpperCase() + "', '" +
-            searchString.toLowerCase() + "'), '" +
-            searchString.toLowerCase() + "'))]]";
-
-        var nodesSnapshot = document.evaluate(query, document,null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-
-        for ( var i=0 ; i < nodesSnapshot.snapshotLength; i++ )
-        {
-            nodesSnapshot.snapshotItem(i).setAttribute(hiddenAttribute, true);
-        }
-    });
-
-    function unHideAll()
-    {
-        document.querySelectorAll('#' + listId + ' [' + hiddenAttribute + ']').forEach(function(hiddenElement) {
-            hiddenElement.removeAttribute(hiddenAttribute);
-        });
-    }
 })(document);
