c0e0fb9a204e2a050e1cdc8f8f4f9b9c1632c1e5
Dima Snisarenko <snisarenkodima@gmail.com>
2017-01-29 14:57:58 +0100

    feat: Add search functionality

--- assets/js/cl.js
+++ assets/js/cl.js
@@ -33,6 +33,86 @@
 
 
 /**
+ * Instant search
+ */
+(function($) {
+    var input = $('#cl-search');
+    var wrapper = input.parents('.dropdown:first');
+    var dropdown = wrapper.find('.dropdown-menu');
+    var likelyIn = 0;
+    var outTimeout = false;
+    var upStrikesOnFirstItem = 0;
+
+    wrapper.focusout(function() {
+        likelyIn = likelyIn - 1;
+
+        if (outTimeout) {
+            clearTimeout(outTimeout);
+        }
+
+        outTimeout = setTimeout(function() {
+            outTimeout = false;
+            if (likelyIn <= 0) {
+                wrapper.removeClass('open');
+                likelyIn = 0;
+            } else {
+                showWhenNeeded();
+            }
+        }, 0);
+    });
+
+    wrapper.focusin(function() {
+        likelyIn = likelyIn + 1;
+        showWhenNeeded();
+    });
+
+    input.keyup(function(event) {
+        // Arrow down pressed
+        if (event.keyCode === 40) {
+            dropdown.find('li:not(.hidden):first a').focus();
+            upStrikesOnFirstItem = 1;
+            return;
+        }
+
+        var searchString = input.val().toLowerCase();
+        dropdown.find('li').each(function() {
+            $(this).toggleClass('hidden', $(this).text().toLowerCase().indexOf(searchString) === -1);
+        });
+
+        showWhenNeeded();
+    });
+
+    dropdown.keyup(function(event) {
+        // Arrow up pressed
+        if (event.keyCode === 38) {
+            if (dropdown.find('a:focus').parent()[0] === dropdown.find('li:visible:first')[0]) {
+                if (++upStrikesOnFirstItem === 2) {
+                    input.focus();
+                    upStrikesOnFirstItem = 0;
+                }
+            }
+        } else {
+            upStrikesOnFirstItem = 0;
+        }
+    });
+
+    dropdown.keydown(function(event) {
+        if (event.keyCode === 13) {
+            input.val('');
+        }
+    });
+
+    function showWhenNeeded() {
+        if (input.val().length === 0 || dropdown.find('li:not(.hidden)').length === 0) {
+            wrapper.removeClass('open');
+        } else {
+            wrapper.addClass('open');
+        }
+    }
+})(jQuery);
+
+
+/**
  * Show/hide code
  */
 (function($) {
